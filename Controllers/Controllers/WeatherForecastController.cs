1778429c636d3a615f46355d93e1329b97907633
Formacion <Formacion@bomberosfloridablanca.com>
2024-08-23 14:54:32 -0500

    Se realizo el codigo del post y el ger de HHTL del api

--- Controllers/Controllers/WeatherForecastController.cs
+++ Controllers/Controllers/WeatherForecastController.cs
@@ -12,21 +12,41 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private static List<WeatherForecast> ListMeatherforecast =new List<WeatherForecast>();
+
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if(ListMeatherforecast == null || !ListMeatherforecast.Any())
+        {
+            ListMeatherforecast= Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+       return ListMeatherforecast;        
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListMeatherforecast.Add(weatherForecast);
+        return Ok();
+    }
+    [HttpDelete]
+    public IActionResult Delete(int index)
+    {
+        ListMeatherforecast.RemoveAt(index);
+        return Ok();
     }
 }
